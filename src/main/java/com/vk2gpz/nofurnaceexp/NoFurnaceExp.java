d2402fbfb6dca31970b43f9d4c23ae06d09004bd
vk2gpz <vk2gpz@gmail.com>
2019-05-12 20:47:22 +1000

    Initial commit.

---
+++ src/main/java/com/vk2gpz/nofurnaceexp/NoFurnaceExp.java
@@ -0,0 +1,54 @@
+package com.vk2gpz.nofurnaceexp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.*;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.inventory.FurnaceExtractEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.*;
+
+import java.util.*;
+
+public class NoFurnaceExp extends JavaPlugin implements CommandExecutor, Listener {
+	private boolean noExp;
+	private static List<Player> GODS = new ArrayList<Player>();
+	
+	@Override
+	public void onEnable() {
+		this.saveDefaultConfig();
+		noExp = getConfig().getBoolean("NoExpMode", true);
+		getCommand("nfexp").setExecutor(this);
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		PluginDescriptionFile pdfile = getDescription();
+		getLogger().info(pdfile.getName() + " version " + pdfile.getVersion() + " is Enabled");
+	}
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdFile = getDescription();
+		getLogger().info(pdFile.getName() + " version " + pdFile.getVersion() + " is Disabled");
+	}
+	
+	
+	@Override
+	public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+		if (s.hasPermission("nfexp.toggle")) {
+			noExp = !noExp;
+			s.sendMessage("§6No exp mode §c" + (noExp ? "enabled" : "disabled") + "§6.");
+		}
+		return true;
+	}
+	
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
+	public void onFurnaceExtract(FurnaceExtractEvent e) {
+		if (noExp) {
+			e.setExpToDrop(0);
+		}
+	}
+}
